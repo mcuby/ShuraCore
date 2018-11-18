46dfd30ece96b97fabaaddec7f7e1b481a3f56da
Roman Shulenkov <postmaster@mcu.by>
2018-11-18 20:01:04 +0300

    [alu, registers] I made a description of all the command for ALU and started the description for working with the register card

--- hls/shura-core-hls/src/alu.h
+++ hls/shura-core-hls/src/alu.h
@@ -6,31 +6,31 @@ public:
 
     //Addition
 	template <class T>
-    T _add (T a, T b) {
+    T _add (T const &a, T const &b) {
 		return a + b;
 	}
 
 	//Subtraction
 	template <class T>
-    T _sub (T a, T b) {
+    T _sub (T const &a, T const &b) {
 		return a - b;
 	}
 
 	//Multiplication
 	template <class T>
-    T _mul (T a, T b) {
+    T _mul (T const &a, T const &b) {
 		return a * b;
 	}
 
 	//Division
 	template <class T>
-    T _div (T a, T b) {
+    T _div (T const &a, T const &b) {
 		return a / b;
 	}
 
 	//Modulo
 	template <class T>
-    T _mod (T a, T b) {
+    T _mod (T const &a, T const &b) {
 		return a % b;
 	}
 
@@ -38,43 +38,80 @@ public:
 
     //Bitwise XOR
     template <class T>
-    T _xor (T a, T b) {
+    T _xor (T const &a, T const &b) {
         return a ^ b;
     }
 
     //Bitwise AND
     template <class T>
-    T _and (T a, T b) {
+    T _and (T const &a, T const &b) {
         return a & b;
     }
 
     //Bitwise OR
     template <class T>
-    T _or (T a, T b) {
+    T _or (T const &a, T const &b) {
         return a | b;
     }
 
     //Bitwise NOT
     template <class T>
-    T _not (T a) {
+    T _not (T const &a) {
         return ~a;
     }
 
     //Bitwise left shift
     template <class T>
-    T _lshift (T a, T b) {
+    T _lshift (T const &a, T const &b) {
         return a << b;
     }
 
     //Bitwise right shift
     template <class T>
-    T _rshift (T a, T b) {
+    T _rshift (T const &a, T const &b) {
         return a >> b;
     }
 
 	//Comparison operators/relational operators
 
+    //Equal to
+    template<class K, class S>
+    bool _equal(K const &a, S const &b) {
+        return a == b;
+    }
 
+    //Not equal to
+    template<class K, class S>
+    bool _nequal(K const &a, S const &b) {
+        return a != b;
+    }
+
+    //Greater than
+    template<class K, class S>
+    bool _greater(K const &a, S const &b) {
+        return a > b;
+    }
+
+    //Less than
+    template<class K, class S>
+    bool _less(K const &a, S const &b) {
+        return a < b;
+    }
+
+    //Greater than or equal to
+    template<class K, class S>
+    bool _gequal(K const &a, S const &b) {
+        return a >= b;
+    }
+
+    //Less than or equal to
+    template<class K, class S>
+    bool _lequal(K const &a, S const &b) {
+        return a <= b;
+    }
 
 };
+
+class ALU globalALU;
+
 
---
+++ hls/shura-core-hls/src/register_file.h
@@ -0,0 +1,211 @@
+#include <stdint.h>
+#include <alu.h>
+
+class REG {
+public:
+
+//basic registers for writing
+    uint64_t w0 = 0;
+    uint64_t w1 = 0;
+    uint64_t w2 = 0;
+    uint64_t w3 = 0;
+    uint64_t w4 = 0;
+    uint64_t w5 = 0;
+    uint64_t w6 = 0;
+    uint64_t w7 = 0;
+
+//basic registers for reading
+    uint64_t r0 = 0;
+    uint64_t r1 = 0;
+    uint64_t r2 = 0;
+    uint64_t r3 = 0;
+    uint64_t r4 = 0;
+    uint64_t r5 = 0;
+    uint64_t r6 = 0;
+    uint64_t r7 = 0;
+
+//stack pointer register
+    uint64_t sp = 0;
+//heap pointer register
+    uint64_t hp = 0;
+//program counter register
+    uint64_t pc = 0;
+//return address register
+    uint64_t ra = 0;
+//exception registers
+    uint64_t er = 0;
+
+//temporary register
+    uint64_t t0 = 0; //for writing data
+    uint64_t t1 = 0; //for reading data
+
+//struct of valid data
+    struct _svd {
+       uint8_t extension: 1;
+       uint8_t register_number : 4;
+       uint8_t reserve : 3;
+    };
+
+    //bool, char, unsigned char, signed char, __int8            1 byte
+    //__int16, short, unsigned short, wchar_t, __wchar_t        2 bytes
+    //float, __int32, int, unsigned int, long, unsigned long	4 bytes
+    //double, __int64, long double, long long                   8 bytes
+
+    //write data
+    void _wd (_svd value, const uint32_t &data) {
+        t0 = 0;
+        switch (value.register_number) {
+        case 0:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w0 = globalALU._or<uint64_t>(w0, t0);
+            }
+            else{
+                w0 = data;
+            }
+            break;
+        case 1:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w1 = globalALU._or<uint64_t>(w1, t0);
+            }
+            else{
+                w1 = data;
+            }
+            break;
+        case 2:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w2 = globalALU._or<uint64_t>(w2, t0);
+            }
+            else{
+                w2 = data;
+            }
+            break;
+        case 3:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w3 = globalALU._or<uint64_t>(w3, t0);
+            }
+            else{
+                w3 = data;
+            }
+            break;
+        case 4:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w4 = globalALU._or<uint64_t>(w4, t0);
+            }
+            else{
+                w4 = data;
+            }
+            break;
+        case 5:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w5 = globalALU._or<uint64_t>(w5, t0);
+            }
+            else{
+                w5 = data;
+            }
+            break;
+        case 6:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w6 = globalALU._or<uint64_t>(w6, t0);
+            }
+            else{
+                w6 = data;
+            }
+            break;
+        case 7:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(data, 32);
+                w7 = globalALU._or<uint64_t>(w7, t0);
+            }
+            else{
+                w7 = data;
+            }
+            break;
+        }
+    }
+
+    //native data reading
+    void _rn (_svd value, const uint32_t *address) {
+        t1 = 0;
+        switch (value.register_number) {
+        case 0:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r0 = globalALU._or<uint64_t>(r0, t0);
+            }
+            else{
+                r0 = *address;
+            }
+            break;
+        case 1:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r1 = globalALU._or<uint64_t>(r1, t0);
+            }
+            else{
+                r1 = *address;
+            }
+            break;
+        case 2:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r2 = globalALU._or<uint64_t>(r2, t0);
+            }
+            else{
+                r2 = *address;
+            }
+            break;
+        case 3:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r3 = globalALU._or<uint64_t>(r3, t0);
+            }
+            else{
+                r3 = *address;
+            }
+            break;
+        case 4:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r4 = globalALU._or<uint64_t>(r4, t0);
+            }
+            else{
+                r4 = *address;
+            }
+            break;
+        case 5:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r5 = globalALU._or<uint64_t>(r5, t0);
+            }
+            else{
+                r5 = *address;
+            }
+            break;
+        case 6:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r6 = globalALU._or<uint64_t>(r6, t0);
+            }
+            else{
+                r6 = *address;
+            }
+            break;
+        case 7:
+            if(value.extension){
+                t0 = globalALU._lshift<uint64_t>(*address, 32);
+                r7 = globalALU._or<uint64_t>(r7, t0);
+            }
+            else{
+                r7 = *address;
+            }
+            break;
+        }
+    }
+};
